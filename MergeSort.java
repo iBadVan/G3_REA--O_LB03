01d0db5d36595988c3e36008237a6588b7343793
iBadVan <ireano@unsa.edu.pe>
2025-04-06 12:59:57 -0500

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,19 @@
 public class MergeSort {
-    
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;  
+        }
+        
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+        
+        mergeSort(left);
+        mergeSort(right);
+        
+    }
+
 }
